533c1126f38cfad070eaff3367aee188a3d206b3
Dexter <bsse1021@iit.du.ac.bd>
2021-07-28 16:44:22 +0600

    Design update product complet

--- src/app/stock-update/stock-update.component.ts
+++ src/app/stock-update/stock-update.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../models/product';
 import { StockUpdateService } from '../stock-update.service';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-stock-update',
@@ -10,19 +12,57 @@ import { StockUpdateService } from '../stock-update.service';
 export class StockUpdateComponent implements OnInit {
 
   pid : string;
+  productForm: FormGroup;
+  searchForm: FormGroup;
   product : Product = new Product();
 
-  constructor(private stockUpdateService : StockUpdateService) { }
+  constructor(private stockUpdateService : StockUpdateService,private _snackBar: MatSnackBar) {
+this.initializeForm();
+  }
 
   ngOnInit(): void {
   }
+   initializeForm() {
+    this.productForm = new FormGroup({
+      pid:new FormControl("", Validators.required),
+      price:new FormControl("", Validators.required),
+      category:new FormControl("", Validators.required),
+      size:new FormControl("", Validators.required),
+      color:new FormControl("", Validators.required),
+      brand:new FormControl("", Validators.required),
+      date_of_entry:new FormControl("", Validators.required),
+    });
+
+    this.searchForm = new FormGroup({
+      pid:new FormControl('', Validators.required),
+    });
+  }
+
+  setFormData() {
+    this.productForm = new FormGroup({
+      pid:new FormControl(this.product.pid, Validators.required),
+      price:new FormControl(this.product.price, Validators.required),
+      category:new FormControl(this.product.category, Validators.required),
+      size:new FormControl(this.product.size, Validators.required),
+      color:new FormControl(this.product.color, Validators.required),
+      brand:new FormControl(this.product.brand, Validators.required),
+      date_of_entry:new FormControl(this.product.date_of_entry, Validators.required),
+    });
+  }
+
+  openSnackBar(message: string, action: string) {
+
+    this._snackBar.open(message, action,{
+      duration: 2000
+    });
+  }
 
   updateItem () : void {
+    this.product = this.productForm.value;
     this.stockUpdateService.updateItem(this.product).subscribe(
       response => {
         if(response)
-          alert("Product updated successfully");
-        
+          this.openSnackBar("Product updated successfully",'close')
         this.product = new Product();
         this.pid=null;
       }
@@ -30,14 +70,17 @@ export class StockUpdateComponent implements OnInit {
   }
 
   searchItem () : void {
+   let temp = this.searchForm.value;
+    this.pid = temp.pid;
     this.stockUpdateService.searchItem(this.pid).subscribe(
       response => {
         if(response == null)
         {
-          alert("Product with this id not found");
+          alert("No Product Found");
           return ;
         }
         this.product= response;
+        this.setFormData();
       }
     )
   }
