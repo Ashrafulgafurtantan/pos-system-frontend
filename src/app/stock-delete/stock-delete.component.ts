3380bc4a6638049c6a0716533d6396f5150c7f22
Dexter <bsse1021@iit.du.ac.bd>
2021-07-28 16:52:34 +0600

    Design delete product complet

--- src/app/stock-delete/stock-delete.component.ts
+++ src/app/stock-delete/stock-delete.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../models/product';
 import { StockDeleteService } from '../stock-delete.service';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {StockUpdateService} from '../stock-update.service';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-stock-delete',
@@ -11,22 +14,67 @@ export class StockDeleteComponent implements OnInit {
 
   pid : string;
   product : Product = new Product();
-  constructor(private stockDeleteService : StockDeleteService) { }
+  productForm: FormGroup;
+  searchForm: FormGroup;
+  constructor(private stockDeleteService : StockDeleteService,private _snackBar: MatSnackBar) {
+    this.initializeForm();
+  }
 
   ngOnInit(): void {
   }
 
+
+  initializeForm() {
+    this.productForm = new FormGroup({
+      pid:new FormControl("", Validators.required),
+      price:new FormControl("", Validators.required),
+      category:new FormControl("", Validators.required),
+      size:new FormControl("", Validators.required),
+      color:new FormControl("", Validators.required),
+      brand:new FormControl("", Validators.required),
+      date_of_entry:new FormControl("", Validators.required),
+    });
+
+    this.searchForm = new FormGroup({
+      pid:new FormControl('', Validators.required),
+    });
+  }
+
+  setFormData() {
+    this.productForm = new FormGroup({
+      pid:new FormControl(this.product.pid, Validators.required),
+      price:new FormControl(this.product.price, Validators.required),
+      category:new FormControl(this.product.category, Validators.required),
+      size:new FormControl(this.product.size, Validators.required),
+      color:new FormControl(this.product.color, Validators.required),
+      brand:new FormControl(this.product.brand, Validators.required),
+      date_of_entry:new FormControl(this.product.date_of_entry, Validators.required),
+    });
+  }
+
+  openSnackBar(message: string, action: string) {
+
+    this._snackBar.open(message, action,{
+      duration: 2000
+    });
+  }
+
+
+
   deleteItem () : void {
+    this.product = this.productForm.value;
     this.stockDeleteService.deleteItem(this.product).subscribe(
       response => {
         if(response)
-          alert("Product deleted successfully");
-        
+          this.openSnackBar("Product deleted successfully",'close');
+
         this.product = new Product();
       }
     )
   }
   searchItem () : void {
+    let temp = this.searchForm.value;
+    this.pid = temp.pid;
     this.stockDeleteService.searchItem(this.pid).subscribe(
       response => {
         if(response == null)
@@ -35,6 +83,7 @@ export class StockDeleteComponent implements OnInit {
           return ;
         }
         this.product= response;
+        this.setFormData();
       }
     )
   }
