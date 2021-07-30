323ca5153a43d5bbbf203b9700df570de8f13663
Dexter <bsse1021@iit.du.ac.bd>
2021-07-30 16:06:04 +0600

    refactored user-ts file

--- src/app/stock-report.service.ts
+++ src/app/stock-report.service.ts
@@ -2,8 +2,6 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Product } from './models/product';
-import { ProductService } from './product.service';
-
 @Injectable({
   providedIn: 'root'
 })
